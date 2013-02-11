5f1b747cb24041095bf6477e181ff8cdad0e309e
Peter Bartels <peterbartels@gmail.com>
2013-02-11 19:25:07 +0000

    added first test

---
+++ Source/GenPres.AcceptanceTests/GenPres.AcceptanceTests/GenPresFitnesseTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenPres.AcceptanceTests
+{   
+    public class GenPresFitnesseTest
+    {
+        public string result { private set; get; }
+
+        public GenPresFitnesseTest()
+        {
+            
+        }
+
+        public string TestGenPresFitnesse()
+        {
+            result = "Fitnesse is working!";
+            return result;
+        }
+
+        public string getResult()
+        {
+            return result;
+        }
+
+    }    
+}
