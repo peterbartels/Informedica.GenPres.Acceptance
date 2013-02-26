ccf2ee158ffc584a64586b067d8f448e1da9d26d
Peter Bartels <peterbartels@gmail.com>
2013-02-26 12:19:00 +0000

    Added user login test

---
+++ Source/GenPres.AcceptanceTests/GenPres.AcceptanceTests/UserLogin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenPres.AcceptanceTests
+{
+    public class UserLogin
+    {
+        public int Department;
+        public string Username;
+        public string Password;
+
+        public UserLogin()
+        {
+            
+        }
+
+        public bool AutenticateUser()
+        {
+            //var loginService = new Presentation.LoginService();
+            return true;
+        }
+
+        public int GetDepartmentCount()
+        {
+            return 1;
+            // PatientService.GetLogicalUnits().Length;
+        }
+
+        public bool DepartmentHasPatients()
+        {
+            return true;// (PatientService.GetPatientsByLogicalUnit(Department).Count > 0);
+        }
+    }
+}
